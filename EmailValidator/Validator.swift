c2740d50df6fed2187bb93919336304b131dccc2
Rohit Sandhu <rohit.sandhu@drishinfo.com>
2024-02-21 17:08:23 +0530

    first commit

---
+++ EmailValidator/Validator.swift
@@ -0,0 +1,15 @@
+//
+//  Validator.swift
+//  EmailValidator
+//
+//  Created by Rohit Sandhu on 21/02/24.
+//
+
+import Foundation
+
+
+public struct Validator {
+   public static func displayFuncValue() {
+        print("Rohit Sandhu Custom Framework")
+    }
+}
